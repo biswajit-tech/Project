f22e79487d66911f98418ac9f16b89f5ec60c7ac
bkulasresth2 <bkulasresth2@IN5CG8411H1S.CSCMWS.CSCMWS.COM>
2019-08-12 09:24:58 +0530

    commit second time

--- GIT-Test-Project/src/absv/firstshjj.java
+++ GIT-Test-Project/src/absv/firstshjj.java
@@ -5,6 +5,7 @@ public class firstshjj {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
  System.out.println("Push code to git repository");
+ System.out.println("dhdhhhd");
 	}
 
 }
