cd25b94d928ca831eb9b3e0ff51177aec5c5952a
bkulasresth2 <bkulasresth2@IN5CG8411H1S.CSCMWS.CSCMWS.COM>
2019-08-12 09:19:31 +0530

    commit code

---
+++ GIT-Test-Project/src/absv/firstshjj.java
@@ -0,0 +1,10 @@
+package absv;
+
+public class firstshjj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("Push code to git repository");
+	}
+
+}
