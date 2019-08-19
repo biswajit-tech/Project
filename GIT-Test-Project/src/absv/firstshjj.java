a9785ee8a3fcc5052385f6fd10fee48e17a98f47
vsavalagi <vsavalagi@INPBDB49P.CSCMWS.CSCMWS.COM>
2019-08-19 14:59:11 +0530

    vinods code

--- GIT-Test-Project/src/absv/firstshjj.java
+++ GIT-Test-Project/src/absv/firstshjj.java
@@ -2,31 +2,9 @@ package absv;
 
 public class firstshjj {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- System.out.println("Push code to git repository");
- System.out.println("dhdhhhd");
- System.out.println("Biswajit ");
-
- System.out.println("Added new line 1482019 8.49pm");
- 
- System.out.println("vinod line");
- 
- System.out.println(" vinods code added here");
- 
- System.out.println("Biswajit added HPEFS QA member");
- 
- 
- 
- System.out.println(""vinod line for edit");
-
-	}
-
-	
-	public void print()
+	public static void main(String[] args) 
 	{
-		//hjh
-		//gjgjh
-		System.out.println("vinod");
+		
+		System.out.println("Vinods code");
 	}
-}
+	}
