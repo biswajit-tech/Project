879a7c0a49f53fc2192cfa243e05d8f69010893b
vsavalagi <vsavalagi@INPBDB49P.CSCMWS.CSCMWS.COM>
2019-08-19 15:45:32 +0530

    new line in class 1

--- GIT-Test-Project/src/absv/Class1.java
+++ GIT-Test-Project/src/absv/Class1.java
@@ -4,5 +4,11 @@ public class Class1 {
 
 	public static void Register() {
 		System.out.println("Register succesfully ");
+		
+		public void print()
+		{
+			
+			System.out.println("vinod code");
+		}
 	}
 }
